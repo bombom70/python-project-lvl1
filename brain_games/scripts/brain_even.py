c2ccaa014f8a2637471e2f25d5c23e72e73543f9
nik <khamdamov_1996@mail.ru>
2021-10-06 16:03:33 +0500

    setting actions

--- brain_games/scripts/brain_even.py
+++
@@ -1,7 +0,0 @@
-def even_game():
-    print('Welcome to the Brain Games')
-    name = prompt.string('May i have your name?')
-    print('Hello, {}'.format(name))
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    question = randint(1, 10)
-    print('Question: {}'.format(question))
